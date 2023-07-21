4cfb3c09f534a0f13bd51ac36ac2818d61932358
Yousuf <yousuf.kuet08@gmail.com>
2023-07-21 07:40:09 +0600

    Add Parse Sdk

--- Crave/AppDelegate/AppDelegate.swift
+++ Crave/AppDelegate/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        initializeParseSdk()
         return true
     }
 
@@ -29,6 +31,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+    
+    private func initializeParseSdk() {
+        let parseConfig = ParseClientConfiguration {
+            $0.applicationId = Keys.applicationId
+            $0.clientKey = Keys.clientKey
+            $0.server = "https://parseapi.back4app.com"
+        }
+        
+        Parse.initialize(with: parseConfig)
     }
 
 
