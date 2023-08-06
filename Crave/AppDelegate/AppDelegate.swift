ab238fd7c45bac84410588816a8be0756f642793
Abu Yousuf <yousuf.kuet08@gmail.com>
2023-08-06 21:58:39 +0600

    Remove parse sdk (#8)

--- Crave/AppDelegate/AppDelegate.swift
+++ Crave/AppDelegate/AppDelegate.swift
@@ -6,7 +6,6 @@
 //
 
 import UIKit
-import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        initializeParseSdk()
+       
         return true
     }
 
@@ -33,16 +32,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
     
-    private func initializeParseSdk() {
-        let parseConfig = ParseClientConfiguration {
-            $0.applicationId = Keys.applicationId
-            $0.clientKey = Keys.clientKey
-            $0.server = "https://parseapi.back4app.com"
-        }
-        
-        Parse.initialize(with: parseConfig)
-    }
-
-
 }
 
